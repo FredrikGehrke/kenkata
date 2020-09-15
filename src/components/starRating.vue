b9a37f387267d66b497c10bdaaba25f53d32c7ad
Marcus Bolinsson <marcusbolinsson@users.noreply.github.com>
2020-09-16 01:09:04 +0200

    starRating component & sass integration

---
+++ src/components/starRating.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-html="stars" class="m-3"></div>
+</template>
+
+<script>
+export default {
+  props: {rating: Number},
+  data() {
+    return {
+      template: "",
+    };
+  },
+  computed: {
+    stars: function() {
+      let i = 0;
+      while (i < this.rating) {
+        let newStars = `<img src="/img/starRating/starSolidIcon.svg" alt="star" />`;
+        this.template = this.template + newStars;
+        i++;
+      }
+      let outlinedStars = 5 - this.rating;
+      i = 0;
+      while (i < outlinedStars) {
+        let newOutlinedStars = `<img src="/img/starRating/starOutlineIcon.svg" alt="star" />`;
+        this.template = this.template + newOutlinedStars;
+        i++;
+      }
+      return this.template;
+    },
+  },
+};
+</script>
+
+<style lang="scss"></style>
