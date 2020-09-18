03b87c79730a1b32eb2edf522232a3d8be575d7d
FredrikGehrke <57892335+FredrikGehrke@users.noreply.github.com>
2020-09-18 10:11:34 +0200

    tog bort gamla owl från script

--- public/js/script.js
+++ public/js/script.js
@@ -1,80 +1,81 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  // AOS INIT
+  AOS.init();
 
-   
-    // AOS INIT
-    AOS.init();
+  // Enables tooltip
+  $('[data-toggle="tooltip"]').tooltip();
 
-    // Enables tooltip
-    $('[data-toggle="tooltip"]').tooltip()
+  // $('.fa-shopping-cart').data('tooltip-custom-class', 'tooltip-cart').tooltip();
+  // $('.fas fa-circle').data('tooltip-custom-class', 'tooltip-circle').tooltip();
 
-    // $('.fa-shopping-cart').data('tooltip-custom-class', 'tooltip-cart').tooltip();
-    // $('.fas fa-circle').data('tooltip-custom-class', 'tooltip-circle').tooltip();
+  // $(document).on('inserted.bs.tooltip', function(e) {
+  //     var tooltip = $(e.target).data('bs.tooltip');
+  //     $(tooltip.tip).addClass($(e.target).data('tooltip-custom-class'));
+  // });
 
-    // $(document).on('inserted.bs.tooltip', function(e) {
-    //     var tooltip = $(e.target).data('bs.tooltip');
-    //     $(tooltip.tip).addClass($(e.target).data('tooltip-custom-class'));
-    // });
-
-
-
-    /// Add a review Form START //
-    function ValidateName()
-    {
-        let letters = /^[A-Za-z]+$/;
-        if (($('#name').val().length < 20 && $('#name').val().length > 2) && ($('#name').val().match(letters))) {
-            $('#name').removeClass('is-invalid')
-            $('#name').addClass('is-valid') 
-            return (true);
-        } else {                               
-            document.getElementById("CannotMsgName").innerHTML = "This field must be less than 20 characters, more than 2 characters and only have letters!";
-            $('#name').addClass('is-invalid')
-            $('#name').focus();
-            return (false);
-        }
+  /// Add a review Form START //
+  function ValidateName() {
+    let letters = /^[A-Za-z]+$/;
+    if (
+      $("#name").val().length < 20 &&
+      $("#name").val().length > 2 &&
+      $("#name").val().match(letters)
+    ) {
+      $("#name").removeClass("is-invalid");
+      $("#name").addClass("is-valid");
+      return true;
+    } else {
+      document.getElementById("CannotMsgName").innerHTML =
+        "This field must be less than 20 characters, more than 2 characters and only have letters!";
+      $("#name").addClass("is-invalid");
+      $("#name").focus();
+      return false;
     }
+  }
 
-    function ValidateReview()
-    {
-        if($('#reviewText').val() === '') {
-            $('#reviewText').addClass('is-invalid'); 
-            $('#reviewText').focus();
-        } else {
-            $('#reviewText').removeClass('is-invalid'); 
-            $('#reviewText').addClass('is-valid'); 
-        }
+  function ValidateReview() {
+    if ($("#reviewText").val() === "") {
+      $("#reviewText").addClass("is-invalid");
+      $("#reviewText").focus();
+    } else {
+      $("#reviewText").removeClass("is-invalid");
+      $("#reviewText").addClass("is-valid");
     }
+  }
 
-    function ValidateEmail() 
-    {
-        let validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-        if (($('#email').val().match(validEmail))) {
-            $('#email').removeClass('is-invalid')
-            $('#email').addClass('is-valid') 
-            return (true)
-        } else {
-            $('#email').addClass('is-invalid')
-            $('#email').focus();
-            return (false)
-        }
+  function ValidateEmail() {
+    let validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if ($("#email").val().match(validEmail)) {
+      $("#email").removeClass("is-invalid");
+      $("#email").addClass("is-valid");
+      return true;
+    } else {
+      $("#email").addClass("is-invalid");
+      $("#email").focus();
+      return false;
     }
+  }
 
-    function ValidateForm()
-    {
-        if ((ValidateReview() === true) && (ValidateName() === true) && (ValidateEmail() === true)) {
-            document.getElementById("SubmittedMsg").innerHTML = "Your review has been submitted, thank you!";
-        } 
-        else {}
+  function ValidateForm() {
+    if (
+      ValidateReview() === true &&
+      ValidateName() === true &&
+      ValidateEmail() === true
+    ) {
+      document.getElementById("SubmittedMsg").innerHTML =
+        "Your review has been submitted, thank you!";
+    } else {
     }
+  }
 
-    $('#reviewForm').submit((e) => {
-        e.preventDefault();
+  $("#reviewForm").submit((e) => {
+    e.preventDefault();
 
-        // ValidateForm()
-        ValidateName() 
-        ValidateReview()
-        ValidateEmail() 
-        ValidateEmail() 
-    });
-    /// Add a review Form END //
-    
-})
+    // ValidateForm()
+    ValidateName();
+    ValidateReview();
+    ValidateEmail();
+    ValidateEmail();
+  });
+  /// Add a review Form END //
+});
