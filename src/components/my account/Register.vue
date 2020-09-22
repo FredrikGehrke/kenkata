01f03f82a522966a37f80d59a607eda138d3658a
doaafarooq <56040568+doaafarooq@users.noreply.github.com>
2020-09-22 10:25:20 +0200

    Login/Register added

---
+++ src/components/my account/Register.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+        <h1>Register</h1>
+        <p>Registering for this site allows you to access your order status and history. Just fill in the fields below, and we’ll get a new account set up for you in no time. We will only ask you for information necessary to make the purchase process faster and easier.</p>
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      </div>
+      <p>Your personal data will be used to support your experience throughout this website, to manage access to your account, and for other purposes described in our privacy policy.
+      </p>
+      <button type="submit" class="btn btn-primary">REGISTER</button>
+    </form>
+
+</div>
+
+
+
+
+</template>
