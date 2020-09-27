4f8965a47786ee0dd446e00e8e39ddb2b199b7fa
ahmadnajjar90 <a7mad.najjar@hotmail.com>
2020-09-27 23:55:04 +0200

    aboutus components

--- src/views/aboutUs.vue
+++ src/views/aboutUs.vue
@@ -1,18 +1,21 @@
 <template>
   <div>
     <aboutHeader/>
+    <aboutStatics/>
     <subscribe/>
   </div>  
 </template>
 
 <script>
 import aboutHeader from '@/components/aboutHeader.vue'
+import aboutStatics from '@/components/aboutStatics.vue'  
 import subscribe from '@/components/subscribe.vue'
 
 export default {
     name: "aboutUs",
     components: {
       aboutHeader,
+      aboutStatics,
       subscribe,
   },
 }
